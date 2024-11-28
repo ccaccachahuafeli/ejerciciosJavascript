a8e07b535fb366bb27829c4b69a49af1395c5ab1
ccaccachahuafeli <ccaccachahuafeliciana@gmail.com>
2024-11-28 16:34:03 -0500

    feat.solucion del ejercicio 9

---
+++ 9.numeroAes/ejercicio.js
@@ -0,0 +1,15 @@
+// escribe tu respuesta acá
+function numeroDeAes(cadena) {
+    let contador = 0;
+    for (let i = 0; i < cadena.length; i++) {
+        if (cadena[i] === 'a') {
+            contador++;
+        }
+    }
+    return contador;
+}
+
+// código de prueba
+console.log(numeroDeAes("abracadabra")) // 5
+console.log(numeroDeAes("etinol")) // 0
+console.log(numeroDeAes("")) // 0
