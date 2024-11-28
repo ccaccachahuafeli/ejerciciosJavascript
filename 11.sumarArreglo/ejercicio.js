63b20561ed3a15f2f07837237ce92f0b80e694a3
ccaccachahuafeli <ccaccachahuafeliciana@gmail.com>
2024-11-28 16:49:08 -0500

    feat:solucion de ejercicio 11

---
+++ 11.sumarArreglo/ejercicio.js
@@ -0,0 +1,12 @@
+//escribe tu respuesta acá
+function sumarArreglo(arreg) {
+    let suma = 0;
+    for (let i = 0; i < arreg.length; i++) {
+        suma += arreg[i];
+    }
+    return suma;
+}
+// código de prueba
+console.log(sumarArreglo([3, 1, 2])) // 6
+console.log(sumarArreglo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // 55
+console.log(sumarArreglo([])) // 0
